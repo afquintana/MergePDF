db17175950e038c890ef2fde96ff55ae2de813e5
afquintana <mail@afquintana.com>
2026-04-29 09:30:26 +0200

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.afquintana.mergepdf"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.2"
+        versionCode = 3
+        versionName = "1.0.3"
     }
 
     buildFeatures {
